fe536c23ea7ca4e6741dc4a08c665643a84fa76a
kaya96 <koda01996@gmail.com>
2018-12-22 15:09:41 -0400

    adding service

---
+++ src/app/service/movie.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class MovieService {
+
+  constructor() { }
+
+  //api key: 0971ed323ba8081b990144eef9e02ace
+  //GET: https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=<API KEY>
+}
